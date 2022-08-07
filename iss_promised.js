385f3b18fa840f85d5917ead5e02e6eee4e1e5ac
Netsanet <netsizebene@gmail.com>
2022-08-07 07:45:03 +0000

    using promises

---
+++ iss_promised.js
@@ -0,0 +1,36 @@
+const request = require ('request-promise-native');
+// iss_promised.js
+
+// ...
+
+/* 
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ * 
+ */
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`http://ipwho.is/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
